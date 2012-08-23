9fd3015b3199298575bcd618abdcd8d091c0f964
vmgray <vmgray@jlab.org>
2012-08-23 23:04:01 +0000

    I edit the code so now it will take a geant simulation and output the Q2 value for all 8 octant combined, and also each individually (all unit are Armstongs). This takes care of having to state what octant the simultaion is being ran in, it can be ran in all the octant and pick out the indivulal octants now. :)

--- analysis/SimQ2.C
+++ analysis/SimQ2.C
@@ -1,10 +1,11 @@
 /*********************************************************** 
 Programmer: Valerie Gray
-Purpose: To Output the Q2 values for simulated data
+Purpose: To Output the Q2 values for simulated data for all the octants
+and each octant individually
 
-Entry Conditions: octant, x and y position , x and y angle direction
+Entry Conditions: x and y position , x and y angle direction
 Date: 04-11-2012
-Modified:
+Modified:08-23-2012
 Assisted By: Wouter Deconinck
 *********************************************************/
 
@@ -17,28 +18,52 @@ Assisted By: Wouter Deconinck
 #include <iostream>
 #include <iomanip>
 
-
-TH1D* q2;
-
-void SimQ2 (int oct, int posx, int posy, int anglex, int angley)
+void SimQ2 (double posx, double posy, int anglex, int angley)
 {
-// groups root files for a run together
-TChain* QweakSimG4_Tree = new TChain ("QweakSimG4_Tree");
+  // groups root files for a run together
+  TChain* QweakSimG4_Tree = new TChain ("QweakSimG4_Tree");
 
-//add the root files to the chain the event_tree branches
-QweakSimG4_Tree->Add(Form("~/QWGEANT4/rootfiles/Octant%d_PositionX_%dmm_PositionY_%dmm_DirectionX_%durad_DirectionY_%durad_3.root",oct,posx,posy, anglex, angley));
+  //add the root files to the chain the event_tree branches
+  QweakSimG4_Tree->Add(Form("~/QWGEANT4/rootfiles/PositionX_%gmm_PositionY_%gmm_DirectionX_%durad_DirectionY_%durad_1.root",posx,posy, anglex, angley));
 
-//define canvas
-TCanvas c1("c1","Sim Q2");
+  //define vector of histograms
+  std::vector<TH1D*> q2;
+  q2.resize(9);
+
+  for (size_t i = 0; i<q2.size();i++)
+  {
+     //set the histogram for the q2 
+     q2[i]= new TH1D (Form("q2[%d]",i),Form("Sim Q2 for Octant number %d",i),100,0.0,0.12);
+  }
 
 //set the histogram for the q2
-q2 = new TH1D ("q2","Q2 value",100,0.0,0.12);
+//q2 = new TH1D ("q2","Q2 value",100,0.0,0.12);
 
-//draw the q2 graph or not
-QweakSimG4_Tree->Draw("Primary.PrimaryQ2>>q2","Primary.CrossSection");
-q2->Draw();
+  //define canvas
+  TCanvas* c1 = new TCanvas ("c1","Sim Q2");
+  c1->Divide(3,3);
 
-//output the values for q2
-cout << "q2: " <<  setprecision(5) << 1000*q2->GetMean() << " error RMS/sqrt(N): " <<  setprecision(4) << 1000*q2->GetRMS()/sqrt(q2->GetEntries()) << endl;
+  //draw the q2 graph for all octants
+  c1->cd(9);
+  QweakSimG4_Tree->Draw("Primary.PrimaryQ2>>q2[0]","Primary.CrossSection");
+
+  //draw the q2 graph for each octants
+  for (size_t oct = 1; oct < q2.size(); oct ++)
+  {
+    c1->cd(oct);
+    QweakSimG4_Tree->Draw(Form("Primary.PrimaryQ2>>q2[%d]",oct),Form("Primary.CrossSection*(Cerenkov.Detector.DetectorID==%d)",oct));
+  }
+
+  //output the values for q2 for all octants together and indidually
+  cout << "All Octants" <<endl;
+  cout << "q2: " <<  setprecision(5) << 1000*q2[0]->GetMean() << " error RMS/sqrt(N): " <<  setprecision(4) << 1000*q2[0]->GetRMS()/sqrt(q2[0]->GetEntries()) << endl;
+
+  for (int oct = 1; oct < 9; oct ++)
+  {
+    cout << "Octant number " << oct <<endl;
+    cout << "q2: " <<  std::setprecision(5) << 1000*q2[oct]->GetMean() << " error RMS/sqrt(N): " <<  std::setprecision(4) << 1000*q2[oct]->GetRMS()/sqrt(q2[oct]->GetEntries()) << endl;
+  }
+
+  return;
 
 }
