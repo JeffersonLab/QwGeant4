0810053c6529460f662bf990ca91849e44dbdaa3
wdconinc <wdconinc@jlab.org>
2015-02-09 18:34:02 +0000

    Work on getting QwGeant4 to compile with geant4.10 (not complete yet).
    The main (annoying) difference between geant4.9 and geant4.10 is that the system of units
    and the physical constants are not loaded by default.  This means that in many places this
    needs to be added by hand.  It also means that innocuous variables like 'm' suddenly start
    generating shadowing errors.  Here are two include files that load the relevant units and
    avoid most shadowing errors.  Next commit will be to fix the variables in the code and
    update the headers.

---
+++ include/QweakSimPhysicalConstants.hh
@@ -0,0 +1,9 @@
+#include <CLHEP/Units/PhysicalConstants.h>
+
+using CLHEP::pi;
+using CLHEP::twopi;
+using CLHEP::halfpi;
+using CLHEP::c_light;
+using CLHEP::electron_charge;
+using CLHEP::h_Planck;
+using CLHEP::hbarc;
