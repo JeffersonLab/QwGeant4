7ff1136c93b14ac1c4ae708d392a934980a3d5af
wdconinc <wdconinc@jlab.org>
2016-03-10 16:33:44 +0000

    Changes to tracking simulation and reconstruction job submission script to allow for a 'tag' in the filename that is independent of the macro name.
    Changes to the macro to allow for specifying targets, raster, position, direction.
    
    New base xml file to do the simulation and reconstruction consecutively taking into
    account that the farm will kill these jobs frequently due to either incompetence or
    malice.

--- scripts/myTrackingRun_submit.sh
+++ scripts/myTrackingRun_submit.sh
@@ -10,16 +10,19 @@
 #
 
 name=myTrackingRun
+tag=_LH2_4x4mm_oct15_dir15x0mrad
 
 #first job number (can change if just want to add
-#on to the get more simultaions)
+#on to the get more simulations)
 firstjob=1
 #total number of jobs one would like done
-lastjob=1
+lastjob=199
 #number of events simulated in each Geant4 file
-nevents=1250
+nevents=200000
 #batch system
 batch=xml
+
+#find geant4 base installation directory
 qwgeant4=`dirname $0`
 qwgeant4=`readlink -f $qwgeant4/..`
 mkdir -p /home/${USER}/scratch
@@ -34,7 +37,7 @@ let jobid=${firstjob}
 	let seedA=10#${unixtime}+${current}+${jobid}
 	let seedB=10#${nanosecond}+${current}+${jobid}
 
-	mkdir -p random/${name}_${jobid}
+	mkdir -p random/${name}${tag}_${jobid}
 	mkdir -p jobs/${batch}
 	mkdir -p macros/jobs
 
@@ -45,36 +48,38 @@ let jobid=${firstjob}
 			-e "s|%seedA%|${seedA}|g" \
 			-e "s|%seedB%|${seedB}|g" \
 			-e "s|%jobid%|${jobid}|g" \
-			-e "s|%name%|${name}|g" \
+			-e "s|%name%|${name}${tag}|g" \
 		macros/${name}.in \
-	> macros/jobs/${name}_${jobid}.mac
+	> macros/jobs/${name}${tag}_${jobid}.mac
 
 	#replace the variables in the file ${basename}.in
 	#which creates the job file that is submitted to the farm
 	sed -e "s|%nevents%|${nevents}|g" \
+			-e "s|%qwanalysis%|${QWANALYSIS}|g" \
+			-e "s|%qwscratch%|${QWSCRATCH}|g" \
 			-e "s|%qwgeant4%|${qwgeant4}|g" \
 			-e "s|%seedA%|${seedA}|g" \
 			-e "s|%seedB%|${seedB}|g" \
 			-e "s|%jobid%|${jobid}|g" \
-			-e "s|%name%|${name}|g" \
+			-e "s|%name%|${name}${tag}|g" \
 			-e "s|%user%|${USER}|g" \
-		jobs/job.${batch}.in \
-	> jobs/${batch}/${name}_${jobid}.${batch}
+		jobs/tracking.${batch}.in \
+	> jobs/${batch}/${name}${tag}_${jobid}.${batch}
 
 	#define where the rootfile will be stored, check to see if a rootfile  
 	#exists, if not then submit the job if it is there do not submit it 
 	#if so then move to the next job
-	rootfile=/mss/home/${USER}/rootfiles/${name}/${name}_${jobid}.root
+	rootfile=/mss/home/${USER}/rootfiles/${name}/${name}${tag}_${jobid}_tracking.root
 	if [ -e ${rootfile} ] ; then
 		echo "File ${rootfile} already exists on mss."
 	else
 		echo "File ${rootfile} doesn't exist. Will submit job."
 		if [ "${batch}" == "xml" ] ; then
-			jsub -xml jobs/${batch}/${name}_${jobid}.${batch}
+			jsub -xml jobs/${batch}/${name}${tag}_${jobid}.${batch}
 		fi
 		if [ "${batch}" == "sh" ] ; then
-			echo "Submitting job jobs/${batch}/${name}_${jobid}.${batch}..."
-			qsub jobs/${batch}/${name}_${jobid}.${batch}
+			echo "Submitting job jobs/${batch}/${name}${tag}_${jobid}.${batch}..."
+			qsub jobs/${batch}/${name}${tag}_${jobid}.${batch}
 		fi
 	fi
 
