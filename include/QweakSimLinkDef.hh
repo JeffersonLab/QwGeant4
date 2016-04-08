a46f8f1ab5ca554baafe8dc1ff4c45b84537c115
wad209 <wsduvall@jlab.org>
2016-04-08 18:48:14 +0000

    This time with all the files

--- include/QweakSimLinkDef.hh
+++ include/QweakSimLinkDef.hh
@@ -82,7 +82,6 @@
 #pragma link C++ class QweakSimUserTungstenPlug_DetectorEvent+;
 
 #pragma link C++ class QweakSimUserCerenkov_MainEvent+;
-#pragma link C++ class QweakSimUserCerenkov_OctantEvent+;
 #pragma link C++ class QweakSimUserCerenkov_DetectorEvent+;
 #pragma link C++ class QweakSimUserCerenkov_RadiatorEvent+;
 #pragma link C++ class QweakSimUserCerenkov_PMTEvent+;
